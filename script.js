aa6f0dc2f0712e9fcde40c715b7dc5c1b666e701
bdhayes01 <briandhayes01@gmail.com>
2025-02-25 11:49:22 -0700

    Might work

--- script.js
+++ script.js
@@ -6,8 +6,46 @@ function validateNumber() {
         errorMessage.style.display = 'block';
         return false;
     }
+    else{
+        store(numberInput);
+    }
 
     errorMessage.style.display = 'none';
     alert('Form submitted successfully!');
     return true;
+}
+
+
+function getLastWeek(){
+    let parsedObj;
+    if(localStorage.getItem('local_pain') !== null) {
+        parsedObj = JSON.parse(localStorage.getItem('local_pain'));
+    }
+    else{
+        return;
+    }
+
+    const list = document.getElementById("last-week");
+
+    const newMap = new Map(Object.entries(parsedObj));
+    Array.from(newMap).reverse().forEach(([key, value]) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${key}: ${value}`;
+        list.appendChild(listItem);
+    });
+}
+function store(e) {
+    let curr_map = new Map();
+    const now = new Date();
+    const currentTime = now.toLocaleTimeString();
+
+    if(localStorage.getItem('local_pain') !== null) {
+        curr_map = JSON.parse(localStorage.getItem('local_pain'));
+        curr_map = new Map(Object.entries(curr_map));
+    }
+    curr_map.set(currentTime, e.value);
+
+    let obj = Object.fromEntries(curr_map);
+
+    localStorage.setItem('local_pain', JSON.stringify(obj))
 }
